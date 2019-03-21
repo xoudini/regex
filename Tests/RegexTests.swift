5eb93b117b98b195aabde60e5de9c3df3b929744
Dan Lindholm <main@xoudini.com>
2019-03-21 17:43:30 +0200

    Set up test target and configured CLI as host

---
+++ Tests/RegexTests.swift
@@ -0,0 +1,18 @@
+//
+//  RegexTests.swift
+//
+
+import XCTest
+
+class RegexTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
