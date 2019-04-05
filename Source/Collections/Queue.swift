a6d6690c999adaca32b267f11b0eb8f803dd8da6
Dan Lindholm <main@xoudini.com>
2019-04-05 23:47:32 +0300

    Documentation, moved reallocation to extension, other small changes

--- Source/Collections/Queue.swift
+++ Source/Collections/Queue.swift
@@ -8,38 +8,67 @@ import Foundation
 ///
 class Queue<Element> {
     
+    /// The internal representation of the queue.
+    ///
+    /// - todo:     Replace with custom array type.
+    ///
     private(set) var representation: [Element]
     
-    var head: Index, tail: Index
+    /// A bounding index for the queue.
+    private var head: Index, tail: Index
     
+    /// The current count of elements in the queue.
     var count: Int {
         return self.tail - self.head
     }
     
+    /// A boolean value indicating whether the queue is empty.
     var isEmpty: Bool {
         return self.count == 0
     }
     
+    /// Designated initializer.
+    ///
+    /// - parameters:
+    ///   - array:  An `Array<Element>` to copy.
+    ///
     init(from array: [Element] = []) {
         self.representation = array
         self.head = 0
         self.tail = array.count
     }
     
+    /// Initializer required by `ExpressibleByArrayLiteral`.
+    ///
     required convenience init(arrayLiteral elements: ArrayLiteralElement...) {
         self.init(from: elements)
     }
     
+    /// Enqueues an element to the tail of the queue.
+    ///
+    /// - parameters:
+    ///   - element:    The element to enqueue.
+    ///
     func enqueue(_ element: Element) {
         self.representation.append(element)
         self.tail += 1
     }
     
+    /// Enqueues a collection to the tail of the queue.
+    ///
+    /// - parameters:
+    ///   - array:      The elements to enqueue.
+    ///
     func enqueue(contentsOf array: [Element]) {
         self.representation.append(contentsOf: array)
         self.tail += array.count
     }
     
+    /// Dequeues the element at the head from the queue.
+    ///
+    /// - returns:      The element at the head, or `nil` if the
+    ///                 queue is empty.
+    ///
     func dequeue() -> Element? {
         guard !self.isEmpty else { return nil }
         defer { self.head += 1 }
--- Source/Collections/UnsafeArray.swift
+++ Source/Collections/UnsafeArray.swift
@@ -4,65 +4,99 @@
 
 import Foundation
 
+/// A generic array implementation using pointers. The array is dynamically resized
+/// when necessary, doubling the capacity for each reallocation.
+///
+/// - warning:  No guarantees are provided -- handle with care.
+///
 class UnsafeArray<Element> {
+    
+    /// A pointer to the start of the allocated memory block.
+    ///
+    /// - todo:     Consider private-only access -- this shouldn't be modified anyways.
+    ///
     private(set) var pointer: UnsafeMutablePointer<Element>
+    
+    /// The current count of elements in the array.
     private(set) var count: Int
+    
+    /// The currently reserved capacity for the array.
     private(set) var capacity: Int
     
+    /// Designated initializer.
+    ///
+    /// - parameters:
+    ///   - capacity:   An integer value to use as the capacity for the initial allocation.
+    ///
+    /// - note:         If not using the default value for the capacity, at least
+    ///                 consider using a power of 2.
+    ///
     init(with capacity: Int = 8) {
         self.pointer = UnsafeMutablePointer<Element>.allocate(capacity: capacity)
         self.count = 0
         self.capacity = capacity
     }
     
-    // TODO: Move to extension of pointer
-    private func reallocate() {
-        let pointer = UnsafeMutablePointer<Element>.allocate(capacity: self.capacity)
-        pointer.moveInitialize(from: self.pointer, count: self.count)
-        self.pointer.deallocate()
-        self.pointer = pointer
-    }
-    
+    /// Inserts an element at the given index.
+    ///
+    /// - parameters:
+    ///   - element:    The elemnt to insert.
+    ///   - index:      The index at which the element should be inserted.
+    ///
+    /// - warning:      Insertion outside of the bounds invokes undefined behaviour.
+    ///
     func insert(_ element: Element, at index: Index) {
-        // TODO: Check bounds
-        
         switch index {
         case self.capacity:
             self.capacity *= 2
-            self.reallocate()
+            self.pointer.reallocate(to: self.capacity, copying: self.count)
             fallthrough
         case self.endIndex:
             defer { self.count += 1 }
             self.pointer.advanced(by: index).initialize(to: element)
         default:
-            self.pointer.advanced(by: index).assign(repeating: element, count: 1)
+            self.pointer.advanced(by: index).assign(element)
         }
     }
     
+    /// Appends an element to the end of the array.
+    ///
+    /// - parameters:
+    ///   - element:    The element to append.
+    ///
     func append(_ element: Element) {
         self.insert(element, at: self.endIndex)
     }
     
+    /// Prevents memory leaks by deallocating the pointer prior ARC cleaning up this instance.
+    ///
     deinit {
         self.pointer.deallocate()
     }
 }
 
+
+// MARK: - RandomAccessCollection
+
 extension UnsafeArray: RandomAccessCollection {
     typealias Index = Int
     
-    var startIndex: Int {
+    var startIndex: Index {
         return 0
     }
     
-    var endIndex: Int {
+    var endIndex: Index {
         return self.count
     }
     
-    subscript (index: Index) -> Element {
+    /// Subscript operator implementation.
+    ///
+    /// - note:     Read-only.
+    /// - warning:  Access outside of the bounds invokes undefined behaviour.
+    ///
+    subscript (position: Index) -> Element {
         get {
-            // TODO: Check bounds
-            return self.pointer.advanced(by: index).pointee
+            return self.pointer.advanced(by: position).pointee
         }
     }
 }
