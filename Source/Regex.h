95f06dd286aa80d440f5b9e1f0886045ba5d790e
Dan Lindholm <main@xoudini.com>
2019-04-09 00:01:45 +0300

    Framework project files

---
+++ Source/Regex.h
@@ -0,0 +1,11 @@
+//
+//  Regex.h
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Regex.
+FOUNDATION_EXPORT double RegexVersionNumber;
+
+//! Project version string for Regex.
+FOUNDATION_EXPORT const unsigned char RegexVersionString[];
