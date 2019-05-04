91a6a8869b3d2f07d927b125294510cbd1f06362
Dan Lindholm <main@xoudini.com>
2019-05-04 18:03:07 +0300

    Inline docs and small cleanup

--- Source/Collections/HashSet.swift
+++ Source/Collections/HashSet.swift
@@ -4,11 +4,91 @@
 
 import Foundation
 
-
+/// A generic recursive node type used as a container for a value.
+///
 fileprivate indirect enum Node<Element> {
+    /// Signifies that the node is empty.
     case empty
+    
+    /// A container for an `Element`, and a link to the next `Node`.
     case bucket(value: Element, next: Node)
 }
+
+
+/// A minimal generic hash set implementation.
+///
+/// - note:     Accepts only types conforming to `Hashable` and `Comparable`.
+///
+public class HashSet<Element: Hashable & Comparable> {
+    
+    /// The internal buffer.
+    private let buffer: UnsafeArray<Node<Element>>
+    
+    /// The size of the buffer.
+    ///
+    /// - warning:      This should be a power of two.
+    ///
+    public let bufferSize: Int
+    
+    /// A bitmask for constraining access into the buffer.
+    private var bufferMask: Int {
+        return self.bufferSize - 1
+    }
+    
+    /// The current count of elements in the set.
+    public private(set) var count: Int
+    
+    /// Designated initializer.
+    ///
+    /// - parameters:
+    ///   - bufferSize: The size of the buffer.
+    ///
+    init(with bufferSize: Int = 512) {
+        self.buffer = UnsafeArray(with: bufferSize, defaultValue: .empty)
+        self.bufferSize = bufferSize
+        self.count = 0
+    }
+    
+    /// Initializes the set from a type conforming to `Sequence`.
+    ///
+    /// - parameters:
+    ///   - sequence:   The sequence to initialize from.
+    ///
+    convenience init<S>(from sequence: S) where S: Sequence, S.Element == Element {
+        self.init()
+        for element in sequence {
+            self.insert(element)
+        }
+    }
+    
+    /// Inserts an element into the set. If the element already exists in
+    /// the set, nothing is done.
+    ///
+    /// - parameters:
+    ///   - element:    The element to insert.
+    ///
+    func insert(_ element: Element) {
+        let index = element.hashValue & self.bufferMask
+        guard !self.contains(element) else { return }
+        self.buffer.insert(.bucket(value: element, next: self.buffer[index]), at: index)
+        self.count += 1
+    }
+    
+    /// A boolean check for whether a given element exists in the set.
+    ///
+    /// - parameters:
+    ///   - element:    The element to search for.
+    ///
+    /// - returns:      A `Bool` signifying whether the element was found.
+    ///
+    func contains(_ element: Element) -> Bool {
+        let index = element.hashValue & self.bufferMask
+        return self.buffer[index].first { $0 == element } != nil
+    }
+}
+
+
+// MARK: - Extensions
 
 extension Node: Sequence {
     
@@ -31,38 +111,6 @@ extension Node: Sequence {
     }
 }
 
-
-public class HashSet<Element: Hashable & Comparable> {
-    private let buffer: UnsafeArray<Node<Element>>
-    public let bufferSize: Int
-    public private(set) var count: Int
-    
-    init(with bufferSize: Int = 512) {
-        self.buffer = UnsafeArray(with: bufferSize, defaultValue: .empty)
-        self.bufferSize = bufferSize
-        self.count = 0
-    }
-    
-    convenience init<S>(from sequence: S) where S: Sequence, S.Element == Element {
-        self.init()
-        for element in sequence {
-            self.insert(element)
-        }
-    }
-    
-    func insert(_ element: Element) {
-        let index = element.hashValue & (self.bufferSize - 1)
-        let root = self.buffer[index]
-        guard root.first(where: { $0 == element }) == nil else { return }
-        self.buffer.insert(.bucket(value: element, next: root), at: index)
-        self.count += 1
-    }
-    
-    func contains(_ element: Element) -> Bool {
-        let index = element.hashValue & (self.bufferSize - 1)
-        return self.buffer[index].first { $0 == element } != nil
-    }
-}
 
 extension HashSet: Sequence {
     
